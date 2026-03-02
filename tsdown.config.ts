6f66ee04b3ff787bac139ac5a33af1c730bb66f7
Eric Mika <eric@ericmika.com>
2026-03-02 09:32:38 -0500

    Initial commit.

---
+++ tsdown.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+	attw: {
+		profile: 'esm-only',
+	},
+	dts: true,
+	fixedExtension: false,
+	platform: 'neutral',
+	publint: true,
+	tsconfig: 'tsconfig.build.json',
+})
