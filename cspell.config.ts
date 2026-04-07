63996ad4dddc33f0c953a8f479b028e21b531b29
Eric Mika <eric@ericmika.com>
2026-04-07 10:38:29 -0400

    Lint and printf.

--- cspell.config.ts
+++ cspell.config.ts
@@ -2,5 +2,5 @@ import { cspellConfig } from '@kitschpatrol/cspell-config'
 
 export default cspellConfig({
 	ignorePaths: ['scratch/*', 'test/assets/obsidian-ts-test-vault/*'],
-	words: ['dbus', 'deadend', 'deadends', 'Dynalist', 'newtab', 'readback', 'tinyexec', 'wordcount'],
+	words: ['deadend', 'deadends', 'Dynalist', 'newtab', 'readback', 'tinyexec', 'wordcount'],
 })
